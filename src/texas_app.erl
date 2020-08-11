dbd03f00a3339019d9b344acdecd754386b44732
Ruan Pienaar <ruan.pienaar@superbet.com>
2020-08-11 14:58:02 +0100

    app env port

--- src/texas_app.erl
+++ src/texas_app.erl
@@ -13,10 +13,11 @@
 
 start(_StartType, _StartArgs) ->
     M = application:get_env(texas, handler_module, texas_example),
+    Port = application:get_env(texas, http_port, 8080),
     Dispatch = cowboy_router:compile([{'_', routes(M)}]),
     {ok, _} = cowboy:start_clear(
         http,
-        [{port, 8080}],
+        [{port, Port}],
         #{ env => #{dispatch => Dispatch} }
     ),
     {ok, _} = texas_sup:start_link().
